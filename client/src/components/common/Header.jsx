aeb27ca285e83fad01f9dd33f4d0e7aabe9eb47b
vishwakarma-vishal <vishal.vishwakarma.dev@gmail.com>
2025-02-02 17:41:11 +0530

    make header responsive and added micro intraction

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,60 +1,62 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useRoutes } from "react-router-dom";
 import useAuth from "../../utils/useAuth";
 
 const Header = () => {
     const navigate = useNavigate();
-    const {auth, logout} = useAuth();
+    const { auth, logout } = useAuth();
+    const location = useLocation();
+
 
     return (
         <header className="flex justify-between items-center px-4 md:px-8 lg:px-10 py-4 bg-white shadow-md">
             <div
-                className="flex items-center gap-x-5 cursor-pointer"
+                className="flex items-center gap-x-2 cursor-pointer"
                 onClick={() => navigate('/')}>
-                <div className="w-8 h-8 sm:w-10 sm:h-10">
-                    <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 6H42L36 24L42 42H6L12 24L6 6Z" fill="currentColor"></path>
-                    </svg>
+                <div className="w-8 h-8 sm:w-12 sm:h-12">
+                    <img src="/logo.svg"></img>
                 </div>
-                <span className="text-xl font-bold">Food Share</span>
+                <span className="text-lg sm:text-xl font-bold">Food Share</span>
             </div>
 
-            <div className="hidden md:block">
-                <nav className="flex gap-x-8 text-base font-semibold">
-                    <a className="cursor-pointer" href="#Work">How it works</a>
-                    <a className="cursor-pointer" href="#WhyUs">Why use FoodShare?</a>
-                    <a className="cursor-pointer" href="#faq">FAQ</a>
-                </nav>
-            </div>
+            {/* only showing the menu item on homepage */}
+            {
+                location.pathname === "/" &&
+                <div className="hidden md:block">
+                    <nav className="flex gap-x-4 lg:gap-x-6 text-base font-semibold text-gray-800">
+                        <a className="cursor-pointer hover:text-black transition-all duration-200" href="#Work">How it works</a>
+                        <a className="cursor-pointer hover:text-black transition-all duration-200" href="#WhyUs">Why FoodShare?</a>
+                        <a className="cursor-pointer hover:text-black transition-all duration-200" href="#faq">FAQs</a>
+                    </nav>
+                </div>
+            }
 
             {auth.isAuthenticated ?
-                <div className="flex gap-4">
+                <div className="flex gap-2 sm:gap-4 text-sm sm:text-base">
                     <button
-                        className="px-4 py-2 bg-green-500 rounded-full text-white font-semibold"
+                        className="px-4 py-2 bg-green-500 hover:bg-green-600 transition-all duration-200 rounded-full text-white font-semibold"
                         onClick={() => navigate(auth.role === "ngo" ? '/ngo-dashboard' : '/restaurant-dashboard')}>
                         Dashboard
                     </button>
                     <button
-                        className="px-4 py-2 border bg-red-500 rounded-full text-white font-semibold"
+                        className="px-4 py-2 bg-red-500 hover:bg-red-600 transition-all duration-200 rounded-full text-white font-semibold"
                         onClick={logout}>
                         Log out
                     </button>
                 </div>
                 :
-                <div className="flex gap-4">
+                <div className="flex gap-2 sm:gap-4 text-sm sm:text-base">
                     <button
-                        className="px-4 py-2 bg-green-500 rounded-full text-white font-semibold"
+                        className="px-4 py-2 border border-green-500 bg-green-500 hover:bg-green-600 transition-all duration-200 rounded-full text-white font-semibold"
                         onClick={() => navigate('/signup')}>
                         Sign up
                     </button>
                     <button
-                        className="px-4 py-2 border border-green-500 rounded-full text-green-500 font-semibold"
+                        className="px-4 py-2 border border-green-500 hover:bg-green-500 transition-all duration-200 hover:text-white rounded-full text-green-500 font-semibold"
                         onClick={() => navigate('/login')}>
                         Log in
                     </button>
                 </div>
             }
-
-
         </header>
     )
 }
