c419af6baa0e0e4d270e665eadff0bad47cb846c
vishwakarma-vishal <vishal.vishwakarma.dev@gmail.com>
2025-03-09 10:55:49 +0530

    fix toast responsiveness

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
-import { RecoilRoot } from 'recoil';
+import { ToastContainer } from 'react-toastify'
+import { RecoilRoot } from 'recoil'
+import 'react-toastify/dist/ReactToastify.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <RecoilRoot>
         <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </RecoilRoot>
-      <ToastContainer 
-      position="bottom-right"/>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
